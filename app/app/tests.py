f0b1069c0f71e1ed799571c8e4d148e5e943e2b0
Sahu <rsahu@ciena.com>
2024-01-27 18:31:24 +0530

    with added tests

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,12 +7,12 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    '''
+    """
     test the calc module
-    '''
+    """
 
     def test_add_numbers(self):
-        '''test adding number function'''
+        """test adding number function"""
         res = calc.add(8, 9)
         self.assertEqual(res, 17)
 
